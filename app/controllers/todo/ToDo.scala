8da466f56816d6cae65f1ef648471b805d2d302a
shizuka-ishizaki <shizuka.ishizaki@nextbeat.net>
2021-05-20 17:30:22 +0900

    ToDo登録画面を作成

--- app/controllers/todo/ToDo.scala
+++ app/controllers/todo/ToDo.scala
@@ -9,16 +9,43 @@ package controllers.todo
 import javax.inject._
 import play.api.mvc._
 import play.api.Configuration
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.I18nSupport
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import lib.persistence.default.{ToDoRepository, ToDoCategoryRepository}
 import model.{ViewValueToDoList, ToDoWithCategory, ViewValueToDoAdd}
 import lib.model.ToDo
+import lib.model.ToDo.ToDoStatus
+import lib.model.ToDoCategory
 import lib.model.ToDoCategory.CategoryColor
 
+/**
+ * 追加・更新用Form
+ */
+case class ToDoFormData (
+  title:      String,
+  body:       String,
+  status:     Short,
+  categoryId: Long,
+)
 
 @Singleton
-class ToDoController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class ToDoController @Inject()(val controllerComponents: ControllerComponents)
+    extends BaseController with I18nSupport {
+
+  /**
+   * ToDo登録用のFormオブジェクト
+   */
+  val toDoForm = Form(
+    mapping(
+      "title"       -> nonEmptyText,
+      "body"        -> nonEmptyText,
+      "state"       -> shortNumber(min = 0, max = 2),
+      "categoryId"  -> longNumber
+    )(ToDoFormData.apply)(ToDoFormData.unapply)
+  )
 
   /**
    * ToDo一覧画面の表示用
@@ -29,6 +56,7 @@ class ToDoController @Inject()(val controllerComponents: ControllerComponents) e
       categorySeq <- ToDoCategoryRepository.all()
     } yield {
       val todoList = todoSeq.map { todo =>
+        val category = categorySeq.find(c => c.id == todo.v.categoryId)
         ToDoWithCategory(
           todo.id,
           todo.v.categoryId,
@@ -47,5 +75,75 @@ class ToDoController @Inject()(val controllerComponents: ControllerComponents) e
       )
       Ok(views.html.todo.List(vv))
     }
+  }
+
+  /**
+   * 登録画面の表示用
+   */
+  def register() = Action async { implicit request: Request[AnyContent] =>
+    for {
+      categorySeq <- ToDoCategoryRepository.all()
+    } yield {
+      val vv = ViewValueToDoAdd(
+        title     = "ToDo登録",
+        cssSrc    = Seq("main.css", "todo/add.css"),
+        jsSrc     = Seq("main.js"),
+        toDoForm  = toDoForm,
+        categorys = categorySeq.map( category =>
+          (category.id.toString -> category.v.name)
+        )
+      )
+      Ok(views.html.todo.Add(vv))
+    }
+  }
+
+  /**
+   * 登録処理
+   */
+  def add() = Action async { implicit request: Request[AnyContent] =>
+    // foldでデータ受け取りの成功、失敗を分岐しつつ処理が行える
+    toDoForm
+      .bindFromRequest()
+      .fold(
+        // 処理が失敗した場合に呼び出される関数
+        // 処理失敗の例: バリデーションエラー
+        (formWithErrors: Form[ToDoFormData]) => {
+          println("kotiきた: " + formWithErrors)
+          for {
+            categorySeq <- ToDoCategoryRepository.all()
+          } yield {
+            val vv = ViewValueToDoAdd(
+              title     = "ToDo登録",
+              cssSrc    = Seq("main.css", "todo/add.css"),
+              jsSrc     = Seq("main.js"),
+              toDoForm  = toDoForm,
+              categorys = categorySeq.map( category =>
+                (category.id.toString -> category.v.name)
+              )
+            )
+            BadRequest(views.html.todo.Add(vv))
+          }
+      },
+
+      // 処理が成功した場合に呼び出される関数
+      (toDoForm: ToDoFormData) => {
+        println("kokoきた")
+        // 登録処理としてSeqに画面から受け取ったコンテンツを持つTweetを追加
+        // 登録が完了したら一覧画面へリダイレクトする
+        val todoWithNoId = new ToDo(
+          id          = None,
+          categoryId  = ToDoCategory.Id(toDoForm.categoryId),
+          title       = toDoForm.title,
+          body        = toDoForm.body,
+          status      = ToDoStatus.apply(toDoForm.status),
+        ).toWithNoId
+        for {
+          _ <- ToDoRepository.add(todoWithNoId)
+        } yield {
+          Redirect(routes.ToDoController.list())
+            .flashing("success" -> "Todoを追加しました!!")
+        }
+      }
+    )
   }
 }
--- app/model/ToDo.scala
+++ app/model/ToDo.scala
@@ -9,13 +9,24 @@ package model
 import lib.model.{ToDo, ToDoCategory}
 import lib.model.ToDo.ToDoStatus
 import lib.model.ToDoCategory.CategoryColor
+import controllers.todo.ToDoFormData
+import play.api.data.Form
 
-// ToDo一覧ページのviewvalue
+// ToDoリスト画面のviewvalue
 case class ViewValueToDoList(
   title:    String,
   cssSrc:   Seq[String],
   jsSrc:    Seq[String],
   toDoList: Seq[ToDoWithCategory],
+) extends ViewValueCommon
+
+// ToDo追加画面のviewvalue
+case class ViewValueToDoAdd(
+  title:      String,
+  cssSrc:     Seq[String],
+  jsSrc:      Seq[String],
+  toDoForm:   Form[ToDoFormData],
+  categorys:  Seq[(String, String)],
 ) extends ViewValueCommon
 
 //todo.listで使用するカテゴリーの情報を持ったcase class
